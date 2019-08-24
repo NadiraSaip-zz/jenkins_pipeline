7a1be8b3ac68439a4dac4e11cf623f188f5c9afc
nsaip <nadiasaip@gmail.com>
2019-08-24 11:13:18 -0500

    updated

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,8 +1,9 @@
 node {
     stage("Say Hello"){
-    properties([parameters([string(defaultValue: 'Nadira', 
-    description: 'please input your name', 
-    name: 'NAME', trim: true)]), pipelineTriggers([cron('* * * * *')])])
+    # properties([parameters([string(defaultValue: 'Nadira', 
+    # description: 'please input your name', 
+    # name: 'NAME', trim: true)]), pipelineTriggers([cron('* * * * *')])])
+    git 'https://github.com/NadiraSaip/jenkins_testrepo.git'
     sh "echo Hello ${NAME}"
     }
 }
