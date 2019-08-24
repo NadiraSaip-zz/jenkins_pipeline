9f3f97ee1112b9400ba3953bf5d3a851eb78fc74
nsaip <nadiasaip@gmail.com>
2019-08-24 11:25:05 -0500

    prod dev qa instances added

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,5 +1,6 @@
 node {
     stage("Say Hello"){
+        properties([parameters([string(defaultValue: '3.16.147.59', description: '''dev: 3.16.147.59 qa: 3.16.147.59 prod: 18.218.24.94''', name: 'Remote_instances', trim: true)])])
         git 'https://github.com/NadiraSaip/jenkins_testrepo.git'
     }
 }
