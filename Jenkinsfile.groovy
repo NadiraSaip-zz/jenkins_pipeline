7a6d751eaada6220b949bd053b1c456f28b8d74d
nsaip <nadiasaip@gmail.com>
2019-08-24 11:29:06 -0500

    updated

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,5 +3,8 @@ node {
         properties([parameters([string(defaultValue: '3.16.147.59', description: '''dev: 3.16.147.59 qa: 3.16.147.59 prod: 18.218.24.94''', name: 'Remote_instances', trim: true)])])
         git 'https://github.com/NadiraSaip/jenkins_testrepo.git'
     }
+    stage("Install Apache"){
+        sh "ssh ec2-user@"${Remote_instances} sudo yum install httpd -y"
+    }
 }
 
