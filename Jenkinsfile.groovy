1aff1680de65e856ea84e46e1ddee09157be40f1
nsaip <nadiasaip@gmail.com>
2019-08-24 11:08:07 -0500

    updated

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,6 +1,8 @@
 node {
     stage("Say Hello"){
-    properties([pipelineTriggers([cron('* * * * *')])])    
+    properties([parameters([string(defaultValue: 'Nadira', 
+    description: 'please input your name', 
+    name: 'NAME', trim: true)]), pipelineTriggers([cron('* * * * *')])])
     sh "echo Hello ${NAME}"
     }
 }
